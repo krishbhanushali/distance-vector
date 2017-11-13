d7d347809b99be58988947e7e92a06f559119199
krishsb2405 <krishsb2405@gmail.com>
2017-11-12 17:11:31 -0800

    bf

--- src/prakash/ram/client/Client.java
+++ src/prakash/ram/client/Client.java
@@ -48,43 +48,42 @@ public class Client extends Thread
         						buffer.flip();
         						while(buffer.hasRemaining()){
         							message+=((char)buffer.get());
-        							System.out.println(message);
-        							ObjectMapper mapper = new ObjectMapper();
-        							mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
-        							Message msg = mapper.readValue(message,Message.class);
-        							//increase the number of received messages counter
-        							String ip = msg.getIpAddress();
-        			        		int fromID = msg.getId();
-        			        		Node myNode = dv.myNode;
-        			        		
-        			        		Map<Node,Integer> receivedRoutingTable = msg.getRoutingTable();
-        			        		Map<Node,Integer> myRoutingTable = dv.routingTable;
-        			        		Iterator myRTEntries = myRoutingTable.entrySet().iterator();
-        			        		Iterator receivedRTEntries = receivedRoutingTable.entrySet().iterator();
-        			        		while(myRTEntries.hasNext()) {
-        			        			while(receivedRTEntries.hasNext()){
-        			            			Entry myRTEntry = (Entry)myRTEntries.next();
-        			            			Entry receivedRTEntry = (Entry)receivedRTEntries.next();
-        			            			
-        			            			Object key1 = (Node)myRTEntry.getKey();
-        			            			Node myRTNode = (Node)key1;
-        			            			
-        			            			Object key2 = (Node)receivedRTEntry.getKey();
-        			            			Node receivedRTNode = (Node)key2;
-        			            			
-        			            			Object value1 = (Integer)myRTEntry.getValue();
-        			            			Integer myRTCost = (Integer)value1;
-        			            			
-        			            			Object value2 = (Integer)receivedRTEntry.getValue();
-        			            			Integer receivedRTCost = (Integer)value2;
-        			            			
-        			            			if(myRTNode.equals(receivedRTNode)){
-        			            				if(receivedRTCost+dv.routingTable.get(receivedRTNode)<myRTCost){
-        			            					dv.routingTable.put(receivedRTNode, receivedRTCost+dv.routingTable.get(receivedRTNode));
-        			            				}
-        			            			}
-        			        			}
-        			        		}
+        						}
+    							ObjectMapper mapper = new ObjectMapper();
+    							mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+    							Message msg = mapper.readValue(message,Message.class);
+    							//increase the number of received messages counter
+    							String ip = msg.getIpAddress();
+    			        		int fromID = msg.getId();
+    			        		Node myNode = dv.myNode;
+    			        		
+    			        		Map<Node,Integer> receivedRoutingTable = msg.getRoutingTable();
+    			        		Map<Node,Integer> myRoutingTable = dv.routingTable;
+    			        		Iterator myRTEntries = myRoutingTable.entrySet().iterator();
+    			        		Iterator receivedRTEntries = receivedRoutingTable.entrySet().iterator();
+    			        		while(myRTEntries.hasNext()) {
+    			        			while(receivedRTEntries.hasNext()){
+    			            			Entry myRTEntry = (Entry)myRTEntries.next();
+    			            			Entry receivedRTEntry = (Entry)receivedRTEntries.next();
+    			            			
+    			            			Object key1 = (Node)myRTEntry.getKey();
+    			            			Node myRTNode = (Node)key1;
+    			            			
+    			            			Object key2 = (Node)receivedRTEntry.getKey();
+    			            			Node receivedRTNode = (Node)key2;
+    			            			
+    			            			Object value1 = (Integer)myRTEntry.getValue();
+    			            			Integer myRTCost = (Integer)value1;
+    			            			
+    			            			Object value2 = (Integer)receivedRTEntry.getValue();
+    			            			Integer receivedRTCost = (Integer)value2;
+    			            			
+    			            			if(myRTNode.equals(receivedRTNode)){
+    			            				if(receivedRTCost+dv.routingTable.get(receivedRTNode)<myRTCost){
+    			            					dv.routingTable.put(receivedRTNode, receivedRTCost+dv.routingTable.get(receivedRTNode));
+    			            				}
+    			            			}
+    			        			}
         						}
         					}
         				}
