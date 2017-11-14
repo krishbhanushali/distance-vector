857b51593ab14acb0fecb7207e681a867bda0f76
krishsb2405 <krishsb2405@gmail.com>
2017-11-13 22:35:33 -0800

    dv algo

--- src/prakash/ram/client/Client.java
+++ src/prakash/ram/client/Client.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -54,21 +55,22 @@ public class Client extends Thread
     			        		Node fromNode = dv.getNodeById(fromID);
     			        		int cost = dv.routingTable.get(fromNode);
     			        		List<String> receivedRT = msg.getRoutingTable();
-    			        		for(String eachReceivedEntry:receivedRT){
-    			        			int id = Integer.parseInt(eachReceivedEntry.split("#")[0]);
-    			        			Node eachNode = dv.getNodeById(id);
-    			        			int costFromMessage = Integer.parseInt(eachReceivedEntry.split("#")[1]);
-	    			        		for(Map.Entry<Node, Integer> entry : dv.routingTable.entrySet()){
-	    			        			if(id == entry.getKey().getId()){
-	    			        				if(dv.neighbors.contains(fromNode) && costFromMessage<cost){
-	    			        					dv.routingTable.put(fromNode,costFromMessage);
-	    			        				}
-	    			        				else if(costFromMessage+cost<dv.routingTable.get(entry.getKey())){
-	    			        					dv.routingTable.put(entry.getKey(),costFromMessage+cost);
-	    			        				}
-	    			        			}
-	    			        		}
+    			        		Map<Node,Integer> createdReceivedRT = makeRT(receivedRT);
+    			        		int neighborCost =0;
+    			        		for(Map.Entry<Node, Integer> entry : dv.routingTable.entrySet()){
+    			        			if(entry.getKey().getIpAddress().equals(msg.getIpAddress())){
+    			        				neighborCost = entry.getValue();
+    			        			}
     			        		}
+    			        		for(Map.Entry<Node, Integer> entry1:createdReceivedRT.entrySet()){
+    			        			for(Map.Entry<Node, Integer> entry2:dv.routingTable.entrySet()){
+    			        				if(entry1.getKey().getId() == entry2.getKey().getId()){
+    			        					if(entry1.getValue()+neighborCost<entry2.getValue()){
+    			        						dv.routingTable.put(entry2.getKey(),entry1.getValue()+neighborCost );
+    			        					}
+    			        				}
+    			        			}
+    			        		}    			        		
         					}
         				}
         			}
@@ -78,6 +80,16 @@ public class Client extends Thread
         }
         
     }
+	private Map<Node, Integer> makeRT(List<String> receivedRT) {
+		Map<Node,Integer> rt = new HashMap<Node,Integer>();
+		for(String str:receivedRT){
+			String[] parts = str.split("#");
+			int id = Integer.parseInt(parts[0]);
+			int cost = Integer.parseInt(parts[1]);
+			rt.put(dv.getNodeById(id), cost);
+		}
+		return rt;
+	}
  
 }
 
